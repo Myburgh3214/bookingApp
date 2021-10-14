7208685cd9651cc6a168344e2c914b6480695514
Myburgh3214 <79691187+Myburgh3214@users.noreply.github.com>
2021-10-14 13:08:06 +0200

    correctec var
    correctec var

--- index.php
+++ index.php
@@ -3,19 +3,21 @@
 <title>bookApp</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/stylesheet.css">
+<head>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+
 <style>
-h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Helvetica, sans-serif}
-body{font-family: Arial, Helvetica, sans-serif}
+  h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Helvetica, sans-serif}
+  body{font-family: Arial, Helvetica, sans-serif}
 </style>
 <body class="w3-light-grey">
 
 <!-- Navigation Bar -->
     
-
 <!-- Page content -->
 <div class="w3-content" style="max-width:1532px;">
 
@@ -24,27 +26,38 @@ body{font-family: Arial, Helvetica, sans-serif}
     <p><b>Put in your requests in the input fields and press the book button and you will be booked at the hotel of your choosing, its as simple as that. If you want to compare all the hotels we provide you with, just simply press the compare button to see the different prices and services etc, from each hotel.</b></p>
   </div>
   <?php if (!$_POST) { ?>
+
     <form action="index.php" method="post">
-      <div class="w3-row-padding">
+
+        <div class="w3-row-padding">
         <div class="w3-col m3">
+
           <label><i class="fa fa-calendar-o"></i> Check In:</label>
           <input class="w3-input w3-border" type="date" placeholder="DD MM YYYY" id="checkIn" name="checkIn" required>
+
         </div>
         <div class="w3-col m3">
+          
           <label><i class="fa fa-calendar-o"></i> Check Out:</label>
           <input class="w3-input w3-border" type="date" placeholder="DD MM YYYY" name="checkOut" id="checkOut" required>
+
         </div>
         <div class="w3-col m2">
+
           <label><i class="fa fa-male"></i> First Name:</label>
           <input class="w3-input w3-border" type="text" placeholder="First Name" name="name" id="name" required>
+
         </div>
-        <div class="w3-col m2">                   
+        <div class="w3-col m2">     
+
           <label><i class="fa fa-child"></i> Last Name:</label>
           <input class="w3-input w3-border" type="text" name="lastName" id="lastName" placeholder="Last Name" required>
+
         </div>
         <div class="w3-col m2">
                 
           <label><i class="fa fa-search"></i> Select your hotel:</label>
+
           <select id="hotelName" name="hotelName">
               <option value="Commodore Hotel" id="commodoreHotel" name="commodoreHotel">Commodore Hotel</option>
               <option value="Southern Sun" id="southernSun" name="southernSun">Southern Sun</option>
@@ -53,10 +66,12 @@ body{font-family: Arial, Helvetica, sans-serif}
         </div>
         </div>
         <div class="buttons"> 
+
           <button type="submit" value="Submit" name="bookBtn" id="bookBtn">BOOK</button>
           <button type="submit" value="Submit" name="compareBtn" id="compareBtn">COMPARE</button>
+
         </div>
-      </div>
+        </div>
     </form>
       <div class="w3-row-padding" id="about">
       <div class="w3-col l4 12">
@@ -64,10 +79,7 @@ body{font-family: Arial, Helvetica, sans-serif}
         <h3>About</h3>
         <h6></h6>
           <p>We accept: <i class="fa fa-credit-card w3-large"></i> <i class="fa fa-cc-mastercard w3-large"></i> <i class="fa fa-cc-amex w3-large"></i> <i class="fa fa-cc-cc-visa w3-large"></i><i class="fa fa-cc-paypal w3-large"></i></p>
-    </div>
-    <div class="w3-col l8 12">
-        
-     
+    </div>      
     </div>
     </div>
   
@@ -78,6 +90,7 @@ body{font-family: Arial, Helvetica, sans-serif}
   </div>
 
   <div class="w3-panel w3-red w3-leftbar w3-padding-32">
+
     <h6><i class="fa fa-info w3-deep-orange w3-padding w3-margin-right"></i> On demand, we can offer playstation, babycall, children care, dog equipment, etc.</h6>
   
   </div>
@@ -88,37 +101,47 @@ body{font-family: Arial, Helvetica, sans-serif}
 </body>
 </html>
 <?php } else {
+
     class Welcomeclass {
       function __construct() {
         echo "Thank you for choosing us";
       }
     }
     
-    $welCon = new Welcomeclass();
+    $welCom = new Welcomeclass();
     $firstName = $_POST["name"];
     $lastName = $_POST["lastName"];
     $checkIn = $_POST["checkIn"];
     $checkOut = $_POST["checkOut"];
     $hotelName = $_POST["hotelName"];
     $footer = $_POST["footer"];
-    echo "<h3><div>First Name: $firstName</div> <div>Last Name: $lastName</div> <div>Checkin Date: $checkIn</div> <div>Checkout Date: $checkOut</div><div> Hotel Name: $hotelName</div>Total = R ";
+
+      echo "<h3><div>First Name: $firstName</div> <div>Last Name: $lastName</div> <div>Checkin Date: $checkIn</div> <div>Checkout Date: $checkOut</div><div> Hotel Name: $hotelName</div>Total = R ";
     
     /// if statement to switch pictures for the two hotels.
     if ( isset( $_POST['bookBtn'] ) ) { 
       $hotelName = $_POST["hotelName"];
-    if ($hotelName === "Commodore Hotel"){
-      $date1=date_create($_POST["checkIn"]);
+        if ($hotelName === "Commodore Hotel"){
+          $date1=date_create($_POST["checkIn"]);
+
           $date2=date_create($_POST["checkOut"]);
+
           $diff=date_diff($date1,$date2);;
           echo $diff->format("%R%a days")*450; 
-      echo "<div><img src='images/commodoreOutside.jpg'/></div>";
+
+           echo "<div><img src='images/commodoreOutside.jpg'/></div>";
+
         }else if($hotelName==="Southern Sun"){
+
           $date1=date_create($_POST["checkIn"]);
+
           $date2=date_create($_POST["checkOut"]);
+
           $diff=date_diff($date1,$date2);;
           echo $diff->format("%R%a days")*500; 
-      echo "<div><img src='images/southernOutside.jpg'/></div>";
-      include('includes/footer.php');       
+
+          echo "<div><img src='images/southernOutside.jpg'/></div>";
+          include('includes/footer.php');       
     }
 }
 }
@@ -128,21 +151,21 @@ body{font-family: Arial, Helvetica, sans-serif}
   <?php
     if ( isset( $_POST['compareBtn'] ) ) { 
 
-    echo "<h4 class= leftsideText><div>Commodore Hotel:</div>
-    <div>Standard Twin Bedroom: R450 per night</div> 
+      echo "<h4 class= leftsideText><div>Commodore Hotel:</div>
+      <div>Standard Twin Bedroom: R450 per night</div> 
       <div>Restaurant:Yes</div>
       <div>Pool: Yes</div>
       <div> Spa: Yes</div>
       </h4>";
       echo "<img src='images/commodoreHotel.jpg'/>";
 
-    echo "<h4 class= rightsideText><div>Southern Hotel Waterfront:</div>
-    <div>Standard Twin Bedroom: R500 per night</div>
+      echo "<h4 class= rightsideText><div>Southern Hotel Waterfront:</div>
+     <div>Standard Twin Bedroom: R500 per night</div>
       <div>Restaurant:Yes Pool: Yes</div> 
       <div>Spa: No</div>
       <div class= right4>Gym: Yes</div>
       </h4>";
-    echo "<img src='images/southernRooms.jpg'/>";
+      echo "<img src='images/southernRooms.jpg'/>";
 
 }
   ?>
